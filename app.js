b3f62839f486af75ae728519265284f6d87e7e7b
Doug Franklin <82669390+CoderFranklin@users.noreply.github.com>
2021-11-29 13:29:07 -0600

    update app.js

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ document.addEventListener('DOMContentLoaded', () =>  {
         zeros = Array(missing).fill(0),
         newRow = zeros.concat(filteredRow);
 
-        squares[i].innerHTML = newRow[0]
-        squares[i +1].innerHTML = newRow[1]
-        squares[i +2].innerHTML = newRow[2]
-        squares[i +3].innerHTML = newRow[3]
+        squares[i].innerHTML = newRow[0];
+        squares[i +1].innerHTML = newRow[1];
+        squares[i +2].innerHTML = newRow[2];
+        squares[i +3].innerHTML = newRow[3];
       }
     }
   }
@@ -53,17 +53,15 @@ document.addEventListener('DOMContentLoaded', () =>  {
   function moveLeft() {
     for (let i=0; i < 16; i++) {
       if (i % 4 === 0) {
-        let totalOne = squares[i].innerHTML
-        let totalTwo = squares[i+1].innerHTML
-        let totalThree = squares[i+2].innerHTML
-        let totalFour = squares[i+3].innerHTML
-        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-
-        let filteredRow = row.filter(num => num)
-        let missing = 4 - filteredRow.length
-        let zeros = Array(missing).fill(0)
-        let newRow = filteredRow.concat(zeros)
-
+        let totalOne = squares[i].innerHTML,
+        totalTwo = squares[i+1].innerHTML,
+        totalThree = squares[i+2].innerHTML,
+        totalFour = squares[i+3].innerHTML,
+        row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)],
+        filteredRow = row.filter(num => num),
+        missing = 4 - filteredRow.length,
+        zeros = Array(missing).fill(0),
+        newRow = filteredRow.concat(zeros);
         squares[i].innerHTML = newRow[0]
         squares[i +1].innerHTML = newRow[1]
         squares[i +2].innerHTML = newRow[2]
@@ -75,17 +73,15 @@ document.addEventListener('DOMContentLoaded', () =>  {
 
   function moveUp() {
     for (let i=0; i < 4; i++) {
-      let totalOne = squares[i].innerHTML
-      let totalTwo = squares[i+width].innerHTML
-      let totalThree = squares[i+(width*2)].innerHTML
-      let totalFour = squares[i+(width*3)].innerHTML
-      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-
-      let filteredColumn = column.filter(num => num)
-      let missing = 4 - filteredColumn.length
-      let zeros = Array(missing).fill(0)
-      let newColumn = filteredColumn.concat(zeros)
-
+      let totalOne = squares[i].innerHTML,
+      totalTwo = squares[i+width].innerHTML,
+      totalThree = squares[i+(width*2)].innerHTML,
+      totalFour = squares[i+(width*3)].innerHTML,
+      column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)],
+      filteredColumn = column.filter(num => num),
+      missing = 4 - filteredColumn.length,
+      zeros = Array(missing).fill(0),
+      newColumn = filteredColumn.concat(zeros);
       squares[i].innerHTML = newColumn[0]
       squares[i +width].innerHTML = newColumn[1]
       squares[i+(width*2)].innerHTML = newColumn[2]
@@ -95,17 +91,15 @@ document.addEventListener('DOMContentLoaded', () =>  {
 
   function moveDown() {
     for (let i=0; i < 4; i++) {
-      let totalOne = squares[i].innerHTML
-      let totalTwo = squares[i+width].innerHTML
-      let totalThree = squares[i+(width*2)].innerHTML
-      let totalFour = squares[i+(width*3)].innerHTML
-      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-
-      let filteredColumn = column.filter(num => num)
-      let missing = 4 - filteredColumn.length
-      let zeros = Array(missing).fill(0)
-      let newColumn = zeros.concat(filteredColumn)
-
+      let totalOne = squares[i].innerHTML,
+      totalTwo = squares[i+width].innerHTML,
+      totalThree = squares[i+(width*2)].innerHTML,
+      totalFour = squares[i+(width*3)].innerHTML,
+      column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)],
+      filteredColumn = column.filter(num => num),
+      missing = 4 - filteredColumn.length,
+      zeros = Array(missing).fill(0),
+      newColumn = zeros.concat(filteredColumn);
       squares[i].innerHTML = newColumn[0]
       squares[i +width].innerHTML = newColumn[1]
       squares[i+(width*2)].innerHTML = newColumn[2]
